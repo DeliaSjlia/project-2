b8ae11ab976e08622d710a3c2393f55b265f3024
Delia <delia.heiz@zkb.ch>
2026-09-15 20:04:26 +0200

    working on homepage, added simple header, main, footer wip

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,8 +1,32 @@
-// src/pages/index.jsx
 export default function HomePage() {
   return (
-    <main>
-      <h1>Welcome to VocaBee 🐝</h1>
-    </main>
+    <>
+      <header>
+        <h2>🐝 VocaBee</h2>
+        <nav>
+          <a href="#">Sprachen</a>
+          <a href="#">Kurse</a>
+          <button type="button">🌙</button>
+          <button type="button">Anmelden</button>
+          <button type="button">Registrieren</button>
+        </nav>
+      </header>
+      <main>
+        <h1>Sprich wie ein Einheimischer.</h1>
+      </main>
+      <footer>
+        <div>
+          <a href="#">
+            <span>🐝</span> VocaBee
+          </a>
+          <nav>
+            <a href="#">Datenschutz</a>
+            <a href="#">Impressum</a>
+            <a href="#">Kontakt</a>
+          </nav>
+          <p>&copy; 2026 VocaBee UG · Berlin</p>
+        </div>
+      </footer>
+    </>
   );
 }
