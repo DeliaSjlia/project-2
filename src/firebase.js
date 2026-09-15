bfd64ecf6f1939a12dd36a3d1bfc6aec72560dcf
Delia <delia.heiz@zkb.ch>
2026-09-15 19:29:23 +0200

    connect firebase

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBzU4vODeIrhVbKYu_Hn01zwOKBcfB7YNY",
+  authDomain: "vocabee-72079.firebaseapp.com",
+  projectId: "vocabee-72079",
+  storageBucket: "vocabee-72079.firebasestorage.app",
+  messagingSenderId: "1059394218959",
+  appId: "1:1059394218959:web:99c2f1d0209a764f6440ba",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
